5662a043e15d570a87bdca925cd74e219cab5b18
unknown <cabrau432@gmail.com>
2020-07-06 00:03:44 -0300

    Projeto finalizado

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleWhatsapp {
+    class Program {
+
+        static void Main (string[] args) {
+            Contato _contato = new Contato ();
+            Agenda agd = new Agenda ();
+            _contato.Nome = "Vash o estouro da boiada";
+            _contato.Telefone = "4325-4576";
+            agd.Cadastrar (_contato);
+            agd.Listar ();
+        }
+    }
+}
